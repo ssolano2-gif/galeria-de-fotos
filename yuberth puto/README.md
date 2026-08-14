00414709c9ced3c8c37a2d2156e07dcb1c8c7ae6
stivens07 <s.solano2@lanuevaamerica.edu.co>
2026-08-14 17:40:42 -0500

    Add files via upload

---
+++ yuberth puto/README.md
@@ -0,0 +1,32 @@
+# Paradise Nursery - Shopping Cart
+
+Aplicación React de una tienda de plantas con carrito de compras.
+
+## Tecnologías
+
+- React
+- Redux Toolkit
+- React Redux
+- Vite
+- GitHub Pages
+
+## Instalación
+
+```bash
+npm install
+npm run dev
+```
+
+## Despliegue
+
+```bash
+npm run deploy
+```
+
+Repositorio:
+
+https://github.com/ssolano2-gif/galeria-de-fotos
+
+Página de GitHub Pages:
+
+https://ssolano2-gif.github.io/galeria-de-fotos/
---
+++ yuberth puto/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import ProductList from "./components/ProductList.jsx";
+import CartItem from "./components/CartItem.jsx";
+
+function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Paradise Nursery</h1>
+        <p>Your online plant shop</p>
+      </header>
+
+      {showCart ? (
+        <CartItem onContinueShopping={() => setShowCart(false)} />
+      ) : (
+        <ProductList onViewCart={() => setShowCart(true)} />
+      )}
+    </div>
+  );
+}
+
+export default App;
